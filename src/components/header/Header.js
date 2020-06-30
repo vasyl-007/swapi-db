bad62feee0c8e988ed21eeb9b0960391da354c5e
Vasyl-007 <vasyl.manyo@gmail.com>
2020-06-30 23:55:09 +0300

    d

---
+++ src/components/header/Header.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => <div>Header component</div>;
+
+export default Header;
---
+++ src/components/personDetails/index.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import "./PersonDetails.css";
+
+export default class index extends Component {
+  render() {
+    return <div>Person Details</div>;
+  }
+}
