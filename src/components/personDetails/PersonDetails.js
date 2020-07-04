cdd3c33b45933433c152c46b7c09835f0af328ed
Vasyl-007 <vasyl.manyo@gmail.com>
2020-07-04 19:23:14 +0300

    d

--- src/components/personDetails/PersonDetails.js
+++ src/components/personDetails/PersonDetails.js
@@ -1,8 +1,28 @@
 import React, { Component } from "react";
 import "./PersonDetails.css";
+import SwapiService from "../../services/SwapiService";
 
 export default class PersonDetails extends Component {
+  swapiService = new SwapiService();
+  state = {
+    person: null,
+  };
+  updatePerson() {
+    const { personId } = this.props;
+    if (!personId) {
+      return;
+    }
+    this.swapiService
+      .getPerson(personId)
+      .then((person) => this.setState({ person }));
+  }
+
   render() {
+    if (!this.state.person) {
+      return <span>Please, select a person from the list</span>;
+    }
+    this.updatePerson(this.props.personId);
+    const { id, name, gender, birthYear, eyeColor } = this.state.person;
     return (
       <div className="person-details card">
         <img
@@ -11,20 +31,20 @@ export default class PersonDetails extends Component {
           alt="card-details"
         />
 
-        <div className="card-body">
-          <h4>R2-D2</h4>
+        <div key={id} className="card-body">
+          <h4>{name}</h4>
           <ul className="list-group list-group-flush">
             <li className="list-group-item">
               <span className="term">Gender</span>
-              <span>male</span>
+              <span>{gender}</span>
             </li>
             <li className="list-group-item">
               <span className="term">Birth Year</span>
-              <span>43</span>
+              <span>{birthYear}</span>
             </li>
             <li className="list-group-item">
               <span className="term">Eye Color</span>
-              <span>red</span>
+              <span>{eyeColor}</span>
             </li>
           </ul>
         </div>
